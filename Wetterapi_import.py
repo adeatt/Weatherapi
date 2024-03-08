a62dc16e0b1370ff4d2bb4921e3db28e4db98220
adeatt <146625599+adeatt@users.noreply.github.com>
2024-03-08 14:28:44 +0100

    Update Wetterapi_import.py
    Removed the function, prints and clean up

--- Wetterapi_import.py
+++ Wetterapi_import.py
@@ -1,32 +1,16 @@
+import requests    
 
+API_KEY = #"here should go the api key which is given by the api provider"
 
+lat = "51.509865"  #London
+lon = "-0.118092"  # London
 
-def wdaten():
-
-    import requests    
-
-    API_KEY = #"here should go the api key which is given by the api provider"
-
-    lat = "51.509865"  #London
-
-    lon = "-0.118092"  # London
-
-    requests_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
-
-    API_answer = requests.get(requests_url) 
-        
-    data = API_answer.json() 
+requests_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
+API_answer = requests.get(requests_url) 
+data = API_answer.json() 
     
-    print(data) 
-    Weather = data["weather"][0]["description"]
-    print(Weather)
-    Tempc = round(data["main"]["temp"]-273.15) # from kelvin to celcius
-    print(Tempc)
-    Windspeed = data["wind"]["speed"]
-    print(Windspeed)
 
-wdaten()
-
-
-
-
+Weather = data["weather"][0]["description"]
+Tempc = round(data["main"]["temp"]-273.15) # from kelvin to celcius
+Windspeed = data["wind"]["speed"]
+print(data)
